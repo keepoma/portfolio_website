cfa3f6abd231cf8ba7c8990cd45c8e4c05b094e6
NikKar <nikita.karuzin@gmail.com>
2023-09-30 12:14:42 -0600

    Functional send mail

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,8 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact form")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Enter mail address")
     user_message = st.text_area("Enter your message")
+    message = f"""\
+Subject: A user has sent you a message
+
+From: {user_email}
+{user_message}
+"""
     button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.markdown("Message sent!")
--- send_email.py
+++ send_email.py
@@ -1,22 +1,15 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "nikita.karuzin@gmail.com"
-password = "tptf zgwd pjdr anmj"
-
-receiver = "nikita.karuzin@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: This is a test
-Please do nos panic!
-Thanks
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+def send_email(message_local):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "nikita.karuzin@gmail.com"
+    password = "bnmu pkmj beki kzfu"
+    receiver = "nikita.karuzin@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message_local)
 
 
